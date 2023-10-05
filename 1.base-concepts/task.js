ceceb857c48cfbbfb42fa60047db082df9a7a500
goga <notifort5@yandex.ru>
2023-10-05 22:56:03 +0300

    first

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,27 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
+   if(a==0){
+      return "Ошибка а=0 недопустимое значение!";
+  }
+  let D = b*b-4*a*c;
+  let x1, x2;
+   if(D>0) {
+     x1 = (-b + Math.sqrt(d) )/(2*a);
+     x2 = (-b - Math.sqrt(d) )/(2*a);
+     arr.push(x1);
+     arr.push(x2);
+     return arr;
+  } 
+    else if(D==0) {
+     x1=-b/(2*a);
+     arr.push(x1);
+      return arr;
+  } 
+    else if(D<0) {
+      return arr;
+  }
   
-  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
