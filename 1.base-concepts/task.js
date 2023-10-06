57f9bf935ab4295db66961f3de64948c0b0f5905
goga <notifort5@yandex.ru>
2023-10-06 20:50:45 +0300

    taskok

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,12 +1,12 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-   if(a==0){
+    if(a==0){
       return "Ошибка а=0 недопустимое значение!";
   }
   let D = b*b-4*a*c;
   let x1, x2;
-   if(D>0) {
+    if(D>0) {
      x1 = (-b + Math.sqrt(d) )/(2*a);
      x2 = (-b - Math.sqrt(d) )/(2*a);
      arr.push(x1);
@@ -21,9 +21,14 @@ function solveEquation(a, b, c) {
     else if(D<0) {
       return arr;
   }
-  
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  let interestRate = percent.toFixed(2);
+  let S = amount - contribution;
+  let rate = interestRate * 1 / 12;
+  let monthPay = S * (rate + (rate / (((1 + rate)**countMonths) - 1)));
+  let result = (monthPay * 12).toFixed(2);
+  return result;
+} 
