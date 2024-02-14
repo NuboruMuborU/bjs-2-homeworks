c2c89f11e714482720a8bff9e4011a2ac7120efd
goga <notifort5@yandex.ru>
2024-02-14 20:06:51 +0300

    task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,9 +1,9 @@
-function getArrayParams(...arr) {
+function getArrayParams(arr) {
   let min = arr[0], max = arr[0], sum = 0, avg = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] > max) max = arr[i];
-    if(arr[i] < min) min = arr[i];
+    if(arr[i] > max) max == arr[i];
+    if(arr[i] < min) min == arr[i];
     sum += arr[i];
   }
 
