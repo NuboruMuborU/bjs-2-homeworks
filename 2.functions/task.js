56f31b26d10dd2144553745d9f5470f5dcdadf9c
goga <notifort5@yandex.ru>
2024-02-18 23:05:59 +0300

    task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -39,6 +39,8 @@ function differenceMaxMinWorker(...arr) {
 function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0, sumOddElement = 0;
 
+  if(arr.length < 1) return 0;
+
   for (let element of arr) {
     if(element % 2 == 0) sumEvenElement += element;
     else sumOddElement += element;
