b7812435ced5c87cb7c7a93172742252d50b8ebc
goga <notifort5@yandex.ru>
2024-02-19 13:19:46 +0300

    task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -4,17 +4,19 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-
     if(users.length < 1) return 0;
-
-    let ages = users
+    
+    if(gender === "мужской" || gender === "женский") {
+        let ages = users
         .filter(users => users.gender === gender)
         .map(users => users.age);
-    
-    let agesSum = ages.reduce((acc, item) => {
-        let sum = acc + item;
-        return sum;
-    });
 
-    return agesSum / ages.length;
+        let agesSum = ages.reduce((acc, item) => {
+            let sum = acc + item;
+            return sum;
+        });
+
+        return agesSum / ages.length;
+    }
+    return 0;
 }
