6210eaba46dabd0e03a488bb454e809136496306
goga <notifort5@yandex.ru>
2024-02-19 22:28:07 +0300

    modified:   1.base-concepts/task.js modified:   2.functions/task.js modified:   3.arrays/task.js modified:   4.objects/task.js modified:   5.classes/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,34 +1,33 @@
 "use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-    if(a==0){
-      return "Ошибка а=0 недопустимое значение!";
-  }
-  let D = b*b-4*a*c;
-  let x1, x2;
-    if(D>0) {
-     x1 = (-b + Math.sqrt(D) )/(2*a);
-     x2 = (-b - Math.sqrt(D) )/(2*a);
-     arr.push(x1);
-     arr.push(x2);
-  } 
-    else if(D==0) {
-     x1=-b/(2*a);
-     arr.push(x1);
-  } 
-     return arr;
+	let arr = [];
+	if (a == 0) {
+		return "Ошибка а=0 недопустимое значение!";
+	}
+	let D = b * b - 4 * a * c;
+	let x1, x2;
+	if (D > 0) {
+		x1 = (-b + Math.sqrt(D)) / (2 * a);
+		x2 = (-b - Math.sqrt(D)) / (2 * a);
+		arr.push(x1);
+		arr.push(x2);
+	} else if (D == 0) {
+		x1 = -b / (2 * a);
+		arr.push(x1);
+	}
+	return arr;
 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let S = amount - contribution;
-  let rate = percent / 100 / 12;
-  let monthPay = S * (rate + (rate / (((1 + rate)**countMonths) - 1)));
-  let result = (monthPay * countMonths);
-  if (result != 0) {
-    return Number(result.toFixed(2));
-  }
-  else {
-    return result;
-  }
-} 
+	let S = amount - contribution;
+	let rate = percent / 100 / 12;
+	let monthPay = S * (rate + (rate / (((1 + rate) ** countMonths) - 1)));
+	let result = (monthPay * countMonths);
+	if (result != 0) {
+		return Number(result.toFixed(2));
+	} else {
+		return result;
+	}
+}
