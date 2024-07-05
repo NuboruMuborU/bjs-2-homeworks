d39eaa4251c6c1875b2bc883179fe4ad0f2592a7
NuboruMuborU <notifort5@yandex.ru>
2024-07-05 18:02:23 +0300

    ghbjhgdbj

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -15,7 +15,7 @@ class PrintEditionItem {
         }
     }
 
-    set state(num) {
+    state(num) {
         if(num >= 100){ 
             this.state = 100;
             return
@@ -27,7 +27,7 @@ class PrintEditionItem {
         this.state = num;
     }
 
-    get state() {
+    state() {
         return this.state;
     }
 }
