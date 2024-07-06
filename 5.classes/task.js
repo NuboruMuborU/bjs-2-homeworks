7d88864d5b1f94c26f79b591186121204feefab9
NuboruMuborU <notifort5@yandex.ru>
2024-07-06 21:15:17 +0300

    kavern

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,3 +1,48 @@
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if(book.state > 30) this.books.push(book);
+    }
+
+    findBookBy(type, value) {
+        for (let index = 0; index < this.books.length; index++) {
+            if(type == "name") {
+                if(value === this.books[index].name) return this.books[index];
+            }
+            if(type == "releaseDate") {
+                if(value === this.books[index].releaseDate) return this.books[index];
+            }
+            if(type == "author") {
+                if(value === this.books[index].author) return this.books[index];
+            }
+            if(type == "type") {
+                if(value === this.books[index].type) return this.books[index];
+            }
+            if(type == "pagesCount") {
+                if(value === this.books[index].pagesCount) return this.books[index];
+            }
+            return null;
+        }
+    }
+
+    giveBookByName(bookName) {
+        let book = null;
+        for(let index = 0; index < this.books.length; index++) {
+            if(this.books[index].name === bookName) {
+                book = this.books[index];
+                this.books.splice(index, 1);
+                break;
+            }
+        }
+        return book;
+    }
+
+}
+
 class PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
         this.name = name;
