fee6074f60b296e259d3899fbffd6d0e28b031b3
NuboruMuborU <notifort5@yandex.ru>
2024-07-12 13:31:42 +0300

    oboaxa

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,12 @@
-﻿
+﻿function parseCount(parse){
+    try {
+        let value = Number.parseFloat(parse);
+        if(isNaN(value)){
+            throw new Error('Невалидное значение');
+        } else{
+            return value;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
