2d9e6ff569a119680d83b78ff5856baf5fc124b2
NuboruMuborU <notifort5@yandex.ru>
2024-07-13 14:07:58 +0300

    oboaha

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,11 +1,19 @@
 ﻿function parseCount(parseUnit) {
     try {
-        if(isNaN(parseUnit) || parseUnit === " ") {
+        if (isNaN(parseUnit) || parseUnit === " ") {
             throw new Error("Невалидное значение");
         } else {
             return Number.parseFloat(parseUnit);
         }
-    } catch(error) {
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function validateCount(validateUnit) {
+    try {
+        parseCount(validateUnit);
+    } catch (error) {
         console.log(error);
     }
 }
