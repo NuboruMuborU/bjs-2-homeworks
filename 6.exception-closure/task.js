2880fee475a8cfdac221c280115f16f6286a2a32
NuboruMuborU <notifort5@yandex.ru>
2024-07-13 16:54:22 +0300

    tozdva

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,13 +1,33 @@
-﻿function parseCount(parse) {
+﻿// function parseCount(parse) {
+//     try {
+//         if (isNaN(Number.parseFloat(parse))) {
+//             throw new Error("Невалидное значение").message;
+//         }
+//         return Number.parseFloat(parse);
+//     } catch (error) {
+//         console.log(error);
+//     }
+    
+// }
+
+// function validateCount(parse) {
+//     try {
+//         return parseFloat(parse);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+
+function parseCount(parse) {
     try {
         if (isNaN(Number.parseFloat(parse))) {
-            throw new Error("Невалидное значение").message;
+            throw new Error("Невалидное значение");
         }
         return Number.parseFloat(parse);
     } catch (error) {
         console.log(error);
     }
-    
 }
 
 function validateCount(parse) {
