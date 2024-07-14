2f2101496374193a84a22f4d7aa6b4ddc0d4c4f0
NuboruMuborU <notifort5@yandex.ru>
2024-07-15 00:00:07 +0300

    mark2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -24,10 +24,6 @@ class Triangle {
         this.a = a;
         this.b = b;
         this.c = c;
-
-        if (a + b > c || a + c > b || b + c > a) {
-            throw new Error("Треугольник с такими сторонами не существует");
-        }
     }
 
     get perimeter() {
@@ -40,7 +36,4 @@ class Triangle {
         let s = ((p * (p - this.a) * (p - this.b) * (p - this.c)) ** 0.5);
         return Number(s.toFixed(3));
     }
-
 }
-
-// hhgg
