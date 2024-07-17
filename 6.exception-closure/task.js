79a9a13fb9810c40078f58638ca24fb194673ed1
NuboruMuborU <notifort5@yandex.ru>
2024-07-17 22:13:48 +0300

    zfffff

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,44 +1,63 @@
 ﻿function parseCount(parse) {
     let value = Number.parseFloat(parse);
+    if(isNaN(value)) {
+    throw new Error("Невалидное значение");
+    }
     return value;
-}
-
-function validateCount(parse) {
+    }
+    
+    function validateCount(parse) {
     try {
-        if (isNaN(parseCount(parse))) {
-            throw new Error("Невалидное значение");
-        }
-        return parseCount(parse);
+    if (isNaN(parseCount(parse))) {
+    throw new Error("Невалидное значение");
+    }
+    return parseCount(parse);
     } catch (error) {
-        return error;
+    return error;
     }
-}
-
-
-class Triangle {
+    }
+    
+    
+    class Triangle {
     constructor(a, b, c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-
-        if (!(a + b > c) || !(a + c > b) || !(b + c > a)) {
-            throw new Error("Треугольник с такими сторонами не существует");
-        }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    
+    if (!(a + b > c) || !(a + c > b) || !(b + c > a)) {
+    throw new Error("Треугольник с такими сторонами не существует");
     }
-
+    }
+    
     get perimeter() {
-        let p = this.a + this.b + this.c;
-        return Number(p);
+    let p = this.a + this.b + this.c;
+    return Number(p);
     }
-
+    
     get area() {
-        let p = this.perimeter / 2;
-        let s = ((p * (p - this.a) * (p - this.b) * (p - this.c)) ** 0.5);
-        return Number(s.toFixed(3));
+    let p = this.perimeter / 2;
+    let s = ((p * (p - this.a) * (p - this.b) * (p - this.c)) ** 0.5);
+    return Number(s.toFixed(3));
     }
-
-}
-
-function getTriangle(a, b, c) {
+    
+    }
+    
+    function getTriangle(a, b, c) {
+    if(!(a + b > c) || !(a + c > b) || !(b + c > a)) {
+    return {
+    a: a,
+    b: b,
+    c: c,
+    
+    get area() {
+    return "Ошибка! Треугольник не существует";
+    },
+    
+    get perimeter() {
+    return "Ошибка! Треугольник не существует";
+    }
+    }
+    } else { 
     return new Triangle(a, b, c);
-}
+    }
+    }
